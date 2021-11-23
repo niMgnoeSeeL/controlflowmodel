a4b5b6f5e5c9afc3559c9cc0f40baf1f844beab2
Seongmin Lee <nim.gnoes.eel@gmail.com>
2021-11-23 18:20:06 +0900

    [Add] Add harder examples

--- example/python/crash.py
+++ example/python/crash.py
@@ -3,4 +3,17 @@ def crashme(s):
         if len(s) > 1 and s[1] == "a":
             if len(s) > 2 and s[2] == "d":
                 if len(s) > 3 and s[3] == "!":
-                    raise Exception()
+                    raise Exception("Deep bug!")
+
+
+def crashme3(s):
+    if len(s) != 15:
+        raise Exception("Wrong input.")
+    if s[0] == "1":
+        crashme(s[3:7])
+    elif s[1] == "1":
+        crashme(s[7:11])
+    elif s[2] == "1":
+        crashme(s[11:15])
+    else:
+        raise Exception("Wrong input.")
--- example/python/triangle.py
+++ example/python/triangle.py
@@ -1,7 +1,9 @@
+FAIL = -1
 NOTATRIANGLE = 0
 SCALENE = 1
 ISOSCELES = 2
 EQUILATERAL = 3
+
 
 def triangle(s):
     if len(s) != 3:
@@ -23,4 +25,19 @@ def triangle(s):
                     result = ISOSCELES
                 else:
                     result = SCALENE
+    return result
+
+
+def triangle3(s):
+    if len(s) != 12:
+        result = FAIL
+    else:
+        if int(s[0]) == 1:
+            result = triangle(s[3:6])
+        elif int(s[1]) == 1:
+            result = triangle(s[6:9])
+        elif int(s[2]) == 1:
+            result = triangle(s[9:12])
+        else:
+            result = FAIL
     return result
