e2f5e24aa2ccfa9cbb5e2fa6d77be8b559bcef88
Seongmin Lee <nim.gnoes.eel@gmail.com>
2021-11-23 18:19:38 +0900

    Add new example: count

---
+++ example/python/count.py
@@ -0,0 +1,11 @@
+def count(s):
+    if len(s) > 10:
+        return -1
+    elif len(s) < 1:
+        return 0
+    else:
+        prev_cnt = count(s[1:])
+        if ord('a') <= ord(s[0]) and ord(s[0]) <= ord('z'):
+            return prev_cnt + 1
+        else:
+            return prev_cnt
